01ad530396a164f26b0a609a7a15fa47e47e22a8
Vinh <vinh.t.ma@gmail.com>
2021-07-18 11:49:47 +0200

    made plot more beautiful

--- convex_polygon_intersection.py
+++ convex_polygon_intersection.py
@@ -1,6 +1,9 @@
-from edge import Edge
-import numpy as np
+import random
+
 import matplotlib.pyplot as plt
+import numpy as np
+
+from edge import Edge
 
 
 def intersect(polygon1, polygon2):
@@ -30,9 +33,9 @@ def _get_vertices_lying_in_the_other_polygon(polygon1, polygon2):
 def _get_edge_intersection_points(polygon1, polygon2):
     intersection_points = list()
     for i in range(len(polygon1)):
-        edge1 = Edge(polygon1[i-1], polygon1[i])
+        edge1 = Edge(polygon1[i - 1], polygon1[i])
         for j in range(len(polygon2)):
-            edge2 = Edge(polygon2[j-1], polygon2[j])
+            edge2 = Edge(polygon2[j - 1], polygon2[j])
             intersection_point = edge1.get_intersection_point(edge2)
             if intersection_point is not None:
                 intersection_points.append(intersection_point)
@@ -41,9 +44,9 @@ def _get_edge_intersection_points(polygon1, polygon2):
 
 def _polygon_contains_point(polygon, point):
     for i in range(len(polygon)):
-        a = np.subtract(polygon[i], polygon[i-1])
-        b = np.subtract(point, polygon[i-1])
-        if np.cross(a,b) < 0:
+        a = np.subtract(polygon[i], polygon[i - 1])
+        b = np.subtract(point, polygon[i - 1])
+        if np.cross(a, b) < 0:
             return False
     return True
 
@@ -52,38 +55,43 @@ def _sort_vertices_anti_clockwise_and_remove_duplicates(polygon, tolerance=1e-7)
     polygon = sorted(polygon, key=lambda p: _get_angle_in_radians(_get_inner_point(polygon), p))
 
     def vertex_not_similar_to_previous(polygon, i):
-        diff = np.subtract(polygon[i-1], polygon[i])
-        return i==0 or np.linalg.norm(diff, np.inf) > tolerance
+        diff = np.subtract(polygon[i - 1], polygon[i])
+        return i == 0 or np.linalg.norm(diff, np.inf) > tolerance
 
     return [p for i, p in enumerate(polygon) if vertex_not_similar_to_previous(polygon, i)]
 
 
 def _get_angle_in_radians(p1, p2):
-    return np.arctan2(p2[1]-p1[1], p2[0]-p1[0])
+    return np.arctan2(p2[1] - p1[1], p2[0] - p1[0])
 
 
 def _get_inner_point(polygon):
     x_coords = [p[0] for p in polygon]
     y_coords = [p[1] for p in polygon]
-    return [(np.max(x_coords)+np.min(x_coords)) / 2.,(np.max(y_coords)+np.min(y_coords)) / 2.]
-
-
-def plot_polygon(polygon):
-    polygon = list(polygon)
-    polygon.append(polygon[0])
-    x,y = zip(*polygon)
-    plt.plot(x,y,'o-')
+    return [(np.max(x_coords) + np.min(x_coords)) / 2., (np.max(y_coords) + np.min(y_coords)) / 2.]
 
 
 if __name__ == '__main__':
 
-    polygon1 = _sort_vertices_anti_clockwise_and_remove_duplicates([[np.cos(x), np.sin(x)] for x in np.random.rand(4)*2*np.pi])
-    polygon2 = _sort_vertices_anti_clockwise_and_remove_duplicates([[np.cos(x), np.sin(x)] for x in np.random.rand(4)*2*np.pi])
+    def generate_random_convex_polygon():
+        return _sort_vertices_anti_clockwise_and_remove_duplicates(
+            [[np.cos(x), np.sin(x)] for x in np.random.rand(random.randint(3, 6)) * 2 * np.pi])
 
+
+    def plot_polygon(polygon):
+        if polygon:
+            _polygon = list(polygon)
+            _polygon.append(_polygon[0])
+            x, y = zip(*_polygon)
+            plt.plot(x, y, 'o-')
+            plt.fill(x, y, alpha=0.25)
+
+
+    polygon1 = generate_random_convex_polygon()
+    polygon2 = generate_random_convex_polygon()
     polygon3 = intersect(polygon1, polygon2)
 
     plot_polygon(polygon1)
     plot_polygon(polygon2)
-    if len(polygon3) > 0:
-        plot_polygon(polygon3)
+    plot_polygon(polygon3)
     plt.show()
